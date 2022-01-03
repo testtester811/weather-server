0d51ad1621a5ae3291c542a35da86e989d00d964
Denys Abert <denys_abert@epam.com>
2022-01-03 07:35:38 -0500

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side js loaded')
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const successElement = document.getElementById('success');
